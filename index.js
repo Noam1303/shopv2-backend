1d5abf49665f9dc536be2affceb7de32f85ec0de
Noam Rabhi-Vidal <nrabhividal@gmail.com>
2025-08-20 17:33:33 +0200

    add cors

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ const corsOptions = {
     credentials: true, // si tu envoies cookies ou headers d’auth
 };
 
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(cors(corsOptions));
 
 app.use(express.json());
